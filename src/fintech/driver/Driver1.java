a24247624ca0bf83fa0c1c3e3ff119f0ad80dff4
DianaManurung <dianamanurung12345@gmail.com>
2025-03-14 16:36:02 +0700

    06

--- src/fintech/driver/Driver1.java
+++ src/fintech/driver/Driver1.java
@@ -1,15 +1,51 @@
 package fintech.driver;
 
+import fintech.model.Account;
+import fintech.model.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S23040 Diana Manurung
+ * @author 12S23047 Jennifer Sihotang
  */
 public class Driver1 {
-
     public static void main(String[] _args) {
+        Scanner sc = new Scanner(System.in);
+        List<Account> account =  new ArrayList<>();
+        List<Transaction> transaction = new ArrayList<>();
 
-        // codes
+        while(true){
+            String input = sc.nextLine();
+            if(input.equals("---")){
+            break;
+            }
+            String[] segments = input.split("#");
+            String command = segments[0];
 
-    }
+            switch (command) {
+                case "create-account":
+                if (segments.length == 5) {
+                create.account(new Account(segments[1], segments[2]), Double.parseDouble(segments[3]));
+                    }
+                    break;
+                case "create-transaction":
+                if (segments.length == 5) {
+                    create.transaction(new Transaction (Integer.parseInt(segments[1],segments[2]),segments[3]), Double.parseDouble(segments[4]), segments[5], segments[6]);
+                }
+                break;
+                case "show-account":
+                        for (Account accounts : account) {
+                            System.out.println(account);
+                        }
+                        break;
+                    }
+                }
+        
+                sc.close();
+            }
+        }
+        
 
-}
--- src/fintech/model/Account.java
+++ src/fintech/model/Account.java
@@ -1,11 +1,46 @@
 package fintech.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S23040 Diana Manurung
+ * @author 12S23047 Jennifer Sihotang
  */
 public class Account {
+    private String accountname;
+    private String owner;
+    private Double balance;
+    private List<Transaction> transactions;
 
-    // class definition
+    public Account(String _accountname, String _owner) {
+        this.accountname = _accountname;
+        this.owner = _owner;
+        this.balance = 0.0; 
+        this.transactions = new ArrayList<>(); 
+    }
 
+    public String getaccountname() {
+        return accountname;
+    }
+
+    public String getowner() {
+        return owner;
+    }
+
+    public Double getbalance() {
+        return balance;
+    }
+
+    public void addTransaction(Transaction transaction) {
+
+        transactions.add(transaction);
+
+        balance += transaction.getAmount();
+    }
+
+    @Override
+    public String toString() {
+        return accountname + "|" + owner + "|" + balance;
+    }
 }
